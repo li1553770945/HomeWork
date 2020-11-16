bf9de457022ee8df307430491bc244d9886cba6f
李 亚宁 <1553770945@qq.com>
2020-11-16 22:29:18 +0800

    数据库配置采用读入方式

--- HomeWork/settings.py
+++ HomeWork/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-
+from common.common import conf
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,10 +76,20 @@ WSGI_APPLICATION = 'HomeWork.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'homework',
+        'USER':conf.get("mysql","username"),
+        'PASSWORD':conf.get("mysql","password"),
+        'HOST':conf.get("mysql","host"),
+        'PORT':'3306',
     }
 }
 
